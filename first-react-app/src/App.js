eca6c280947fc67b32380805e901bb56db3792da
nikunj-var <nikunjvar2002@gmail.com>
2023-07-29 17:00:19 +0530

    learning styles

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 
 function App() {
   // without using variables
-
   // return <div className="App">
   //   <h3>Full Name : Nikunj Varshney</h3>
   //   <p>Age : 21</p>
@@ -11,7 +10,6 @@ function App() {
   // </div>;
 
   // using variables
-
   // const firstName = "Nikunj";
   // const lastName = "Varshney";
   // const age = 21;
@@ -25,7 +23,6 @@ function App() {
   // );
 
   // using functions
-
   const firstName = "Nikunj";
   const lastName = "Varshney";
   const age = 21;
@@ -35,20 +32,93 @@ function App() {
   };
   const placeholder = "enter your details";
   const BlogObj = {
-    title : "Blog Title 1",
-    description : "lorem epsum dolor lorem epsum dolor lorem epsum dolor lorem epsum dolor lorem epsum dolor "
+    title: "Blog Title 1",
+    description:
+      "lorem epsum dolor lorem epsum dolor lorem epsum dolor lorem epsum dolor lorem epsum dolor ",
   };
+
+  {
+    /* storing style in variables */
+  }
+  const styles = {
+    margin: "16px",
+    padding: "16px",
+    boxSizing: "border-box",
+    borderRadius: "5px",
+    boxShadow: "0 2px 5px #ccc",
+  };
+
+  // creating array of contents
+
+  const blogarr = [
+    {
+      id: 1,
+      title: "Blog Title 3",
+      description:
+        "lorem epsum dolor lorem epsum dolor lorem epsum dolor lorem epsum dolor lorem epsum dolor ",
+    },
+    {
+      id: 2,
+      title: "Blog Title 4",
+      description:
+        "lorem epsum dolor lorem epsum dolor lorem epsum dolor lorem epsum dolor lorem epsum dolor ",
+    },
+    {
+      id: 3,
+      title: "Blog Title 5",
+      description:
+        "lorem epsum dolor lorem epsum dolor lorem epsum dolor lorem epsum dolor lorem epsum dolor ",
+    },
+  ];
+
+  // fetching content from array using map
+  const blogCards = blogarr.map((item) => {
+    return (
+      <div className="blogcard" key={item.id}>
+        <h3>{item.title}</h3>
+        <p>{item.description}</p>
+      </div>
+    );
+  });
   return (
     <div className="App">
-      <h3>Full Name : {getfullname(firstName, lastName)}</h3>
-      <p>Age : {age}</p>
-      <p>Job : {Job}</p>
-      <input placeholder={placeholder}></input>
+      {/* adding styles - Internal styles */}
+      <div
+        style={{
+          margin: "16px",
+          padding: "16px",
+          boxSizing: "border-box",
+
+          borderRadius: "5px",
+          boxShadow: "0 2px 5px #ccc",
+        }}
+      >
+        <h3>Full Name : {getfullname(firstName, lastName)}</h3>
+        <p>Age : {age}</p>
+        <p>Job : {Job}</p>
+        <input placeholder={placeholder}></input>
+      </div>
+
       <hr></hr>
-      <div>
+
+      {/* adding internal css using variables */}
+      <div style={styles}>
         <h3>{BlogObj.title}</h3>
         <p>{BlogObj.description}</p>
       </div>
+
+      <hr></hr>
+
+      {/* giving styles using external css */}
+      <div className="blogcard">
+        <h3>{BlogObj.title}</h3>
+        <p>{BlogObj.description}</p>
+      </div>
+
+      <hr></hr>
+
+      {/* generating components dynamically */}
+      {blogCards}
     </div>
   );
 }
